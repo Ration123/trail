51d17c1f66130db7aaae460efc3193fee87e852e
Ration123 <smartvendingmachinemit@gmail.com>
2025-04-27 00:24:16 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,12 +1,9 @@
 import streamlit as st
 from utils import set_background, show_title_image, get_translator
 
-def app():
-    # Setup
+def app(lang_toggle):
     set_background()
-    lang_toggle = st.sidebar.checkbox("Switch to Tamil", key="lang_toggle_checkbox")
     t = get_translator(lang_toggle)
-
     # Display the title image and header
     show_title_image()
     st.header(t("Language Switcher"))
