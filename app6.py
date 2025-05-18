0ecf06b794684f18e70e27e25218dd5bc47916b3
Ration123 <smartvendingmachinemit@gmail.com>
2025-05-18 15:39:22 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
+from utils import set_background, get_translator
 
 # Initialize Firebase once
 if not firebase_admin._apps:
