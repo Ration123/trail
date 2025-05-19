35e4d32d7e18ba952712b73a78781dc5db707435
Ration123 <smartvendingmachinemit@gmail.com>
2025-05-19 23:14:45 +0530

    Update app8.py

--- app8.py
+++ app8.py
@@ -1,16 +1,20 @@
 import streamlit as st
+from utils import set_background, get_translator
 
 def app(lang_toggle):
-    st.title("📝 New Ration Shop Registration")
+    set_background()
+    t = get_translator(lang_toggle)
 
-    st.markdown("""
+    st.title(t("📝 New Ration Shop Registration"))
+
+    st.markdown(t("""
         If you are a new user and want to register for ration shop services, 
         please fill the form below:
-    """)
+    """))
 
     st.markdown("""
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSek8ehEkLq3d8dt95Mz_CaOSBcnWYXxoDLpNAL-qpuV3cQXuA/viewform?embedded=true" 
-                width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">
+                width="100%" height="1400" frameborder="0" marginheight="0" marginwidth="0">
                 Loading…
         </iframe>
     """, unsafe_allow_html=True)
