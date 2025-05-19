a7b49122a3bbe4b89dae5b034cb14c6e143429b4
Ration123 <smartvendingmachinemit@gmail.com>
2025-05-19 22:49:23 +0530

    Create app8.py

---
+++ app8.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def new_registration_app():
+    st.set_page_config(page_title="New Ration Shop Registration", page_icon="📝")
+
+    st.title("📝 New Ration Shop Registration")
+    st.markdown("""
+        Welcome to the Ration Shop Registration Page.  
+        Please fill out the form below to apply for a new ration shop registration.  
+        Ensure all details are correct before submitting.
+    """)
+
+    # Replace this URL with your actual Google Form link
+    google_form_url = "https://docs.google.com/forms/d/e/1FAIpQLSdXXXXXXXXXXXXXXX/viewform?embedded=true"
+
+    # Embed the form using iframe
+    st.components.v1.html(
+        f"""
+        <iframe src="{google_form_url}" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">
+        Loading…
+        </iframe>
+        """,
+        height=950,
+    )
+
+
